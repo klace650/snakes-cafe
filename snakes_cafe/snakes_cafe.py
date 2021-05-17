0fb93111159a55172f4715eefbd53ac7f55c5373
Klace Koch <koch.klace@gmail.com>
2021-05-17 15:23:51 -0700

    header started

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,1 +1,8 @@
-print("hello? world?")
+print("""
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+""")
