d820a4bdef84d94eb068b6b222032191af40ad95
Klace Koch <koch.klace@gmail.com>
2021-05-22 19:58:49 -0700

    added dev branch

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -8,19 +8,28 @@ intro = ("""
 **************************************
 """)
 
-outro = ("""
+order_prompt = ("""
 ***********************************
 ** What would you like to order? **
 ***********************************
 """)
 print(intro)
+input("> ")
 
+appetizers = {
+  "Wings":0,
+  "Cookies":0,
+  "Spring Rolls":0,
+  }
 
-appetizers = ["Wings", "Cookies", "Spring Rolls"]
-entrees = ["Salmon","Steak","Meat Tornado","A Literal Garden"]
-deserts = ["Ice Cream","Cake","Pie"]
-drinks = ["Coffee","Tea", "Unicorn Tears"]
+# appetizers['Wings'] += 1
 
-print(appetizers,entrees,deserts, drinks)
-print(outro)
+# entrees = {"Salmon","Steak","Meat Tornado","A Literal Garden"}
+# deserts = ["Ice Cream","Cake","Pie"]
+# drinks = ["Coffee","Tea", "Unicorn Tears"]
+
+print(appetizers
+# ,entrees,deserts, drinks
+)
+print(order_prompt)
 
